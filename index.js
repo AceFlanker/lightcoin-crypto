a7e2a7ba25d07e95b42f40e0ac4104597dfcb01b
Yuan (Daniel) Li <86581841+AceFlanker@users.noreply.github.com>
2021-08-28 01:52:37 -0700

    Trasaction history and withdrawal limit added

--- index.js
+++ index.js
@@ -1,29 +1,81 @@
-let balance = 500.00;
+class Account {
 
-class Withdrawal {
-
-  constructor(amount) {
-    this.amount = amount;
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
   }
 
-  commit() {
-    balance -= this.amount;
+  get balance() {
+    // .reduce() is very "buggy" on finding stuff within this.transanctions...
+    let sum = 0;
+    for (const trans of this.transactions) {
+      sum += trans.value;
+    }
+    return sum;
+  }
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
   }
 
 }
 
+class Transaction {
 
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
+  commit() {
+    if (this.isAllowed) {
+      this.time = new Date();
+      this.account.addTransaction(this);
+      return true;
+    }
+    return false;
+  }
+}
 
+class Withdrawal extends Transaction {
+  get isAllowed() {
+    if (this.amount < this.account.balance) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  get value() {
+    return -this.amount;
+  }
+}
+
+class Deposit extends Transaction {
+  get isAllowed() {
+    return true;
+  }
+  get value () {
+    return this.amount;
+  }
+}
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("snow-patrol");
+
+t1 = new Deposit(50.25, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
+// console.log('Transaction 1:', t1, t1.commit());
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
+// console.log('Transaction 2:', t2, t2.commit());
 
-console.log('Balance:', balance);
+t3 = new Deposit(120.00, myAccount);
+t3.commit();
+// console.log('Transaction 3:', t3, t3.commit());
+
+t4 = new Withdrawal(121.00, myAccount);
+t4.commit();
+// console.log('Transaction 4:', t4, t4.commit());
+
+console.log('Balance:', myAccount.balance);
